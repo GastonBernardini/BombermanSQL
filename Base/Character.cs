45e035f74bc370fc29ba4ed6f3050d9adde4a9a6
GastonBernardini <124274459+GastonBernardini@users.noreply.github.com>
2023-10-01 23:49:17 -0300

    Tile Colitions

--- Base/Character.cs
+++ Base/Character.cs
@@ -13,6 +13,14 @@ namespace MyGame
         private float movementTimer;
         private float currentTimer;
         private IntPtr image;
+        private int tilex;
+        private int tiley;
+        private bool isCollidingRight = false;
+        private bool isCollidingLeft = false;
+        private bool isCollidingUp = false;
+        private bool isCollidingDown = false;
+        public int TileX => tilex;
+        public int TileY => tiley;
 
         //private Animation currentAnimation;
         //private Animation idleAnimation;
@@ -27,28 +35,28 @@ namespace MyGame
 
         public void Update()
         {
-
-            int tilex = (int)Transform.Position.x / TileMap.Instance.TileSize;
-            int tiley = (int)Transform.Position.y / TileMap.Instance.TileSize;
-
+            
+             tilex = (int)Transform.Position.x / TileMap.Instance.TileSize;
+             tiley = (int)Transform.Position.y / TileMap.Instance.TileSize;
+             IsColliding();
             if (currentTimer == movementTimer)
             {
-                if (Engine.KeyPress(Engine.KEY_LEFT))
+                if (Engine.KeyPress(Engine.KEY_LEFT)&& !isCollidingLeft)
                 {
                     transform.Translate(new Vector2(-1, 0), speed);
                     currentTimer = 0;
                 }
-                if (Engine.KeyPress(Engine.KEY_RIGHT))
+                if (Engine.KeyPress(Engine.KEY_RIGHT)&& !isCollidingRight)
                 {
                     transform.Translate(new Vector2(1, 0), speed);
                     currentTimer = 0;
                 }
-                if (Engine.KeyPress(Engine.KEY_UP))
+                if (Engine.KeyPress(Engine.KEY_UP)&& !isCollidingUp)
                 {
                     transform.Translate(new Vector2(0, -1), speed);
                     currentTimer = 0;
                 }
-                if (Engine.KeyPress(Engine.KEY_DOWN))
+                if (Engine.KeyPress(Engine.KEY_DOWN)&& !isCollidingDown)
                 {
                     transform.Translate(new Vector2(0, 1), speed);
                     currentTimer = 0;
@@ -75,6 +83,45 @@ namespace MyGame
         public void Render()
         {
             Engine.Draw(image, transform.Position.x, transform.Position.y);
+        }
+        private void IsColliding()
+        {
+            if (TileMap.Instance.Tiles1[tilex +1,tiley] != 0)
+            {
+                isCollidingRight = true;
+                Console.WriteLine("colisiona derecha");
+            }
+            else
+            {
+                isCollidingRight = false;
+            }
+            if (TileMap.Instance.Tiles1[tilex - 1, tiley] != 0)
+            {
+                isCollidingLeft = true;
+                Console.WriteLine("colisiona izquierda");
+            }
+            else
+            {
+                isCollidingLeft = false;
+            }
+            if (TileMap.Instance.Tiles1[tilex , tiley +1 ] != 0)
+            {
+                isCollidingDown = true;
+                Console.WriteLine("colisiona abajo");
+            }
+            else
+            {
+                isCollidingDown = false;
+            }
+            if (TileMap.Instance.Tiles1[tilex , tiley - 1] != 0)
+            {
+                isCollidingUp = true;
+                Console.WriteLine("colisiona arriba");
+            }
+            else
+            {
+                isCollidingUp = false;
+            }
         }
     }
 }
---
+++ Base/Colitions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame
+{
+    internal class Colitions
+    {
+        private Character player = Program.player;
+        private bool isCollidingRight = false;
+        private bool isCollidingLeft = false;
+        private bool isCollidingUp = false;
+        private bool isCollidingDown = false;
+
+        public bool IsCollidingRight => isCollidingRight;
+        public bool IsCollidingLeft => isCollidingLeft;
+        public bool IsCollidingUp => isCollidingUp;
+        public bool IsCollidingDown => isCollidingDown;
+
+
+        private void IsColliding()
+        {
+            if ( TileMap.Instance.Tiles1[player.TileX +1, player.TileY] != 0 ) 
+            { 
+                isCollidingRight = true;
+                Console.WriteLine("colisiona derecha");
+            }
+
+        }
+
+    }
+}
