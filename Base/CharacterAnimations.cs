dfddd591b6b7cfe61011f2813eccf83f953e6606
GastonBernardini <124274459+GastonBernardini@users.noreply.github.com>
2023-11-28 14:21:10 -0300

    Nega-Bomberman joins the fight

--- Base/CharacterAnimations.cs
+++ Base/CharacterAnimations.cs
@@ -70,7 +70,7 @@ namespace MyGame
                 List<IntPtr> Player2RightTextures = new List<IntPtr>();
                 for (int i = 0; i < 4; i++)
                 {
-                    IntPtr frame = Engine.LoadImage($"assets/Player/B_Right/B_Right{i}.png");
+                    IntPtr frame = Engine.LoadImage($"assets/Player/NB_Right/NB_Right{i}.png");
                     Player2RightTextures.Add(frame);
                 }
                 rightAnimation = new Animation("WalkRight", Player2RightTextures, 0.2f, true);
@@ -78,7 +78,7 @@ namespace MyGame
                 List<IntPtr> Player2LeftTextures = new List<IntPtr>();
                 for (int i = 0; i < 4; i++)
                 {
-                    IntPtr frame = Engine.LoadImage($"assets/Player/B_Left/B_Left{i}.png");
+                    IntPtr frame = Engine.LoadImage($"assets/Player/NB_Left/NB_Left{i}.png");
                     Player2LeftTextures.Add(frame);
                 }
                 leftAnimation = new Animation("WalkLeft", Player2LeftTextures, 0.2f, true);
@@ -86,7 +86,7 @@ namespace MyGame
                 List<IntPtr> Player2UpTextures = new List<IntPtr>();
                 for (int i = 0; i < 5; i++)
                 {
-                    IntPtr frame = Engine.LoadImage($"assets/Player/B_Up/B_Up{i}.png");
+                    IntPtr frame = Engine.LoadImage($"assets/Player/NB_Up/NB_Up{i}.png");
                     Player2UpTextures.Add(frame);
                 }
                 upAnimation = new Animation("WalkUp", Player2UpTextures, 0.2f, true);
@@ -94,7 +94,7 @@ namespace MyGame
                 List<IntPtr> Player2DownTextures = new List<IntPtr>();
                 for (int i = 0; i < 4; i++)
                 {
-                    IntPtr frame = Engine.LoadImage($"assets/Player/B_Down/B_Down{i}.png");
+                    IntPtr frame = Engine.LoadImage($"assets/Player/NB_Down/NB_Down{i}.png");
                     Player2DownTextures.Add(frame);
                 }
                 downAnimation = new Animation("WalkDown", Player2DownTextures, 0.2f, true);
@@ -102,7 +102,7 @@ namespace MyGame
                 List<IntPtr> Player2IdleTextures = new List<IntPtr>();
                 for (int i = 0; i < 1; i++)
                 {
-                    IntPtr frame = Engine.LoadImage($"assets/Player/B_Idle/B_Idle{i}.png");
+                    IntPtr frame = Engine.LoadImage($"assets/Player/NB_Idle/NB_Idle{i}.png");
                     Player2IdleTextures.Add(frame);
                 }
                 idleAnimation = new Animation("WalkLeft", Player2IdleTextures, 0.2f, true);
