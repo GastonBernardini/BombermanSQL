1d98a693f042039feef66d32325ce3cec26051e0
GastonBernardini <124274459+GastonBernardini@users.noreply.github.com>
2023-11-28 02:49:33 -0300

    Pick up

---
+++ Base/MoreBombs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame
+{
+    internal class MoreBombs : GameObject,  IsPickupeable
+    {
+        private Animation morebombsAnimation;
+        public MoreBombs (Vector2 pos) : base(pos)
+        {
+
+        }
+        protected override void CreateAnimations()
+        {
+            List<IntPtr> morebombsTextures = new List<IntPtr>();
+            for (int i = 0; i <= 1; i++)
+            {
+                IntPtr frame = Engine.LoadImage($"assets/Bomb/AddBomb_{i}.png");
+                morebombsTextures.Add(frame);
+            }
+            morebombsAnimation = new Animation("MoreBombs", morebombsTextures, 0.2f, false);
+        }
+        public override void Update()
+        {
+
+        }
+
+        public override void Render()
+        {
+
+        }
+        public void Pickup()
+        {
+            Engine.Debug("¡Tienes una bomba extra!");
+            GameManager.Instance.levelController.gameObjectList.Remove(this);
+        }
+    }
+}
---
+++ Base/SpeedUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame
+{
+    internal class SpeedUp : GameObject, IsPickupeable
+    {
+        private Animation speedAnimation;
+        public SpeedUp(Vector2 pos) : base(pos)
+        {
+
+        }
+        protected override void CreateAnimations()
+        {
+            List<IntPtr> speedTextures = new List<IntPtr>();
+            for (int i = 0; i <= 1; i++)
+            {
+                IntPtr frame = Engine.LoadImage($"assets/speed_{i}.png");
+                speedTextures.Add(frame);
+            }
+            speedAnimation = new Animation("Morespeed", speedTextures, 0.2f, false);
+        }
+        public override void Update()
+        {
+
+        }
+
+        public override void Render()
+        {
+
+        }
+        public void Pickup()
+        {
+            Engine.Debug("¡Tienes mas velocidad de movimiento!");
+            GameManager.Instance.levelController.gameObjectList.Remove(this);
+        }
+    }
+
+
+
+
+
+
+}
+
